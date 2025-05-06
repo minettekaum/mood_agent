6d74d7699a1737f04909478233f3b36b334389cb
minettebrink <mimmi.brink@gmail.com>
2025-05-06 15:35:18 +0200

    trying it out with a small model

--- fetch_data.py
+++ fetch_data.py
@@ -1,15 +1,40 @@
 from dotenv import load_dotenv
 import os
 import requests 
+from datetime import datetime, timedelta
+
+today = datetime.now().date()
+yesterday = today - timedelta(days=1)
+today_str = today.strftime('%Y-%m-%d')
+yesterday_str = yesterday.strftime('%Y-%m-%d')
+
 
 load_dotenv('.env')
-url = 'https://api.ouraring.com/v2/usercollection/daily_activity' 
-params={ 
-    'start_date': '2021-11-01', 
-    'end_date': '2021-12-01' 
-}
-headers = { 
+
+
+def fetch_data_sleep_readiness(url):
+    params={ 
+    'start_date': today_str, 
+    'end_date': today_str 
+    }
+    headers = { 
   'Authorization': f'Bearer {os.getenv("TOKEN")}' 
-}
-response = requests.request('GET', url, headers=headers, params=params) 
-print(response.text)
+    }
+    response = requests.request('GET', url, headers=headers, params=params) 
+    return response.json()
+
+def fetch_data_activity(url):
+    params={ 
+    'start_date': yesterday_str, 
+    'end_date': today_str
+    }
+    headers = { 
+  'Authorization': f'Bearer {os.getenv("TOKEN")}' 
+    }
+    response = requests.request('GET', url, headers=headers, params=params) 
+    return response.json()
+
+
+
+
+   
